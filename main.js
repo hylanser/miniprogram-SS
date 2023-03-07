25dba56eabed77be791423bb040bd14938c42d49
hylanser <57030956+hylanser@users.noreply.github.com>
2023-03-07 23:17:40 +0800

    add home page

--- main.js
+++ main.js
@@ -16,6 +16,35 @@ app.$mount()
 // #ifdef VUE3
 import { createSSRApp } from 'vue'
 import App from './App.vue'
+
+// 导入网络请求的包
+import { $http } from '@escook/request-miniprogram'
+
+uni.$http = $http
+
+//请求根路径
+$http.baseUrl = 'http://api-hmugo-web.itheima.net'
+// 请求拦截器：请求开始之前做一些事情
+$http.beforeRequest = function(option){
+  uni.showLoading({
+    title:'数据加载中...'
+  })
+}
+
+// 响应拦截器：请求完成之后做一些事情
+$http.afterRequest = function () {
+  uni.hideLoading()
+}
+
+// 封装弹框方法
+uni.$showMsg = function(title = '数据请求失败!', duration = 1500) {
+  uni.showToast({
+    title,
+    duration,
+    icon: 'none'
+  })
+}
+
 export function createApp() {
   const app = createSSRApp(App)
   return {
