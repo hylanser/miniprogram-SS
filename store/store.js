8b366c726f29c816bcf6b0c30823127888f4b89b
hylanser <57030956+hylanser@users.noreply.github.com>
2023-03-14 22:36:40 +0800

    add address

--- store/store.js
+++ store/store.js
@@ -5,6 +5,9 @@ import Vuex from 'vuex'
 // 2.导入购物车的 vuex 模块
 import moduleCart from './cart.js'
 
+// 导入用户的 vuex 模块
+import moduleUser from './user.js'
+
 // 3. 将 Vuex 安装为 Vue 的插件 Vue3不需要
 /* Vue.use(Vuex) */
 
@@ -15,6 +18,8 @@ const store = new Vuex.Store({
     // 挂载购物车的 vuex 模块，模块内成员的访问路径被调整为 m_cart，例如：
     // 购物车模块中 cart 数组的访问路径是 m_cart/cart
     m_cart: moduleCart,
+    // 挂载用户的 vuex 模块，访问路径为 m_user
+    m_user: moduleUser,
   },
 })
 
